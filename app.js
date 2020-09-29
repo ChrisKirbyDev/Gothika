681895107861187646414939ab67e17ec30f3d05
Chris <chriskirbydev@gmail.com>
2020-09-29 13:31:24 +1300

    Create board function to render a 100 square grid

--- app.js
+++ app.js
@@ -12,4 +12,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const rotateButton = document.querySelector("#rotate");
   const turnDisplay = document.querySelector("#whose-go");
   const infoDisplay = document.querySelector("#info");
+  const userSquares = [];
+  const computerSquares = [];
+
+  const width = 10;
+
+  //Create Board
+  function createBoard(grid, squares) {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement("div");
+      square.dataset.id = i;
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+
+  createBoard(userGrid, userSquares);
+  createBoard(computerGrid, computerSquares);
 });
