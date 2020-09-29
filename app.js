bcaab79270cc08d0a8fb6c1287794bd6a9113dbc
Chris <chriskirbydev@gmail.com>
2020-09-29 17:18:30 +1300

    stop components being dropped outside of the field

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
   ];
 
   // Draw the computers ships in random locations
+
   function generate(ship) {
     let randomDirection = Math.floor(Math.random() * ship.directions.length);
     let current = ship.directions[randomDirection];
@@ -107,6 +108,7 @@ document.addEventListener("DOMContentLoaded", () => {
   generate(shipArray[4]);
 
   // rotate the ships
+
   function rotate() {
     if (isHorizontal) {
       destroyer.classList.toggle("destroyer-container-vertical");
@@ -132,6 +134,7 @@ document.addEventListener("DOMContentLoaded", () => {
   rotateButton.addEventListener("click", rotate);
 
   //move around user ship
+
   ships.forEach((ship) => ship.addEventListener("dragstart", dragStart));
   userSquares.forEach((square) =>
     square.addEventListener("dragstart", dragStart)
@@ -187,17 +190,111 @@ document.addEventListener("DOMContentLoaded", () => {
     let shipLastId = lastShipIndex + parseInt(this.dataset.id);
     console.log(shipLastId);
 
+    const notAllowedHorizontal = [
+      0,
+      10,
+      20,
+      30,
+      40,
+      50,
+      60,
+      70,
+      80,
+      90,
+      1,
+      11,
+      21,
+      31,
+      41,
+      51,
+      61,
+      71,
+      81,
+      91,
+      2,
+      22,
+      32,
+      42,
+      52,
+      62,
+      72,
+      82,
+      92,
+      3,
+      13,
+      23,
+      33,
+      43,
+      53,
+      63,
+      73,
+      83,
+      93,
+    ];
+
+    const notAllowedVertical = [
+      99,
+      98,
+      97,
+      96,
+      95,
+      94,
+      93,
+      92,
+      91,
+      90,
+      89,
+      88,
+      87,
+      86,
+      85,
+      84,
+      83,
+      82,
+      81,
+      80,
+      79,
+      78,
+      77,
+      76,
+      75,
+      74,
+      73,
+      72,
+      71,
+      70,
+      69,
+      68,
+      67,
+      66,
+      65,
+      64,
+      63,
+      62,
+      61,
+      60,
+    ];
+
+    let newNotAllowedHorizontal = notAllowedHorizontal.splice(
+      0,
+      10 * lastShipIndex
+    );
+    let newNotAllowedVertical = notAllowedVertical.splice(
+      0,
+      10 * lastShipIndex
+    );
+
     selectedShipIndex = parseInt(selectedShipNameWithIndex.substr(-1));
 
     shipLastId = shipLastId - selectedShipIndex;
 
-    if (isHorizontal) {
+    if (isHorizontal && !newNotAllowedHorizontal.includes(shipLastId)) {
       for (let i = 0; i < draggedShipLength; i++) {
         userSquares[
           parseInt(this.dataset.id) - selectedShipIndex + i
         ].classList.add("taken", shipClass);
       }
-    } else if (!isHorizontal) {
+    } else if (!isHorizontal && !newNotAllowedVertical.includes(shipLastId)) {
       for (let i = 0; i < draggedShipLength; i++) {
         userSquares[
           parseInt(this.dataset.id) - selectedShipIndex + width * i
@@ -208,5 +305,7 @@ document.addEventListener("DOMContentLoaded", () => {
     displayGrid.removeChild(draggedShip);
   }
 
-  function dragEnd() {}
+  function dragEnd() {
+    console.log("dragend");
+  }
 });
